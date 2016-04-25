2da70835cac817a37e349c01d8d39aa6bb5ee2a2
AlexVirtuoZz <sanya_bovkun@ukr.net>
2016-04-26 02:56:29 +0300

    refactored.mvc

--- src/my/project/Controller.java
+++ src/my/project/Controller.java
@@ -1,6 +1,6 @@
 package my.project;
 
-
+import java.io.BufferedReader;
 import java.util.Scanner;
 
 /**
@@ -25,61 +25,47 @@ public class Controller {
     /**
      * First, we set a random number
      * Then we welcome our user and show him bounds of a random number
-     * After user's finally won, we greet him and show, how much steps it took
+     * After user's finally won, we greet him and show statistics
+     * @see my.project.Model#step
      */
     public void game() {
         model.setValue(model.rand());
         view.printMessage(view.WELCOME);
         view.printMessage(view.ranging(model.getLeft_border(),model.getRight_border()));
-        while (!step()){
-            step();
+
+        int scValue = inputIntValueWithScanner(sc);
+        while (!model.step(scValue)){
+            directUser(scValue);
+            view.printMessage(view.ranging(model.getLeft_border(),model.getRight_border()));
+            view.statistics.append(view.stepStatistics(model.getCounter(), model.getValue(), scValue));
+            scValue = inputIntValueWithScanner(sc);
         }
+
         view.printMessage(view.CONGRATS);
         view.printMessage(view.statistics.toString());
         view.printMessage(view.statistics(model.getCounter()));
     }
 
     // The Utility methods
-    //A method to input number with scanner. If input is wrong, we point it out and repeat an attempt
+
+    /**A method to input number with scanner.
+     *  If input is wrong, we point it out and repeat an attempt
+     *  If input muber is out of bounds, we point it out and repeat an attempt
+     */
     public int inputIntValueWithScanner(Scanner sc) {
-        while (!sc.hasNextInt()){
-            view.printMessage(view.WRONG_INPUT);
+        while (!sc.hasNextInt() || sc.nextInt() >= model.getRight_border() || sc.nextInt() <= model.getLeft_border()){
+            if (!sc.hasNextInt()) view.printMessage(view.WRONG_INPUT);
+            else {
+                view.printMessage(view.OUT_OF_BOUNDS);
+                view.printMessage(view.ranging(model.getLeft_border(), model.getRight_border()));}
             sc.next();}
         return sc.nextInt();
     }
 
-    /**
-     * A method describes one player's step
-     * First we check, if number is out of bounds
-     * Then, if random number is bigger then user's, we set new bound and repeat a step
-     * If user's number is equals to a random one, user wins
-     * And if it's not, we know that random number is smaller, we set new bound and repeat a step
-     * Counter counts, how many steps has user done
-     * @return
-     */
-    boolean step() {
-        model.increaseCounter();
-
-        model.setAttempt(inputIntValueWithScanner(sc));
-
-        view.statistics.append(view.stepStatistics(model.getCounter(), model.getValue(), model.getAttempt()));
-
-        if (model.getAttempt() >= model.getRight_border() || model.getAttempt() <= model.getLeft_border()){
-            view.printMessage(view.OUT_OF_BOUNDS);
-            view.printMessage(view.ranging(model.getLeft_border(), model.getRight_border()));
-            return false;}
-
-        else if (model.isBigger(model.getValue(), model.getAttempt())){
-            view.printMessage(view.NUMBER_BIGGER);
-            model.setLeft_border(model.getAttempt());}
-
-        else if (model.equals(model.getValue(), model.getAttempt()))
-            return true;
-
-        else {model.setRight_border(model.getAttempt());
-        view.printMessage(view.NUMBER_SMALLER);}
-        view.printMessage(view.ranging(model.getLeft_border(), model.getRight_border()));
-
-        return false;
+    //A method to direct user between an input and a random value
+    public void directUser(int scValue) {
+        if (model.isBigger(scValue, model.getValue()))  view.printMessage(view.NUMBER_SMALLER);
+        else  view.printMessage(view.NUMBER_BIGGER);
     }
+
 }
